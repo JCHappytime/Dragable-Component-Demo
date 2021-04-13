b6444eae13d101e1f6cd876691ad7aa70fabbe02
Jessica Jiang <18782959613@163.com>
2021-04-13 10:04:07 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import '@/custom-component' // 自定义组件
 
+import '@/assets/iconfont/iconfont.css'
+import '@/styles/animate.css'
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/styles/reset.css'
+
+Vue.use(ElementUI, { size: 'small' })
 Vue.config.productionTip = false
 
 new Vue({
